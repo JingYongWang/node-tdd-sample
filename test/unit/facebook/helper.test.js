985bda020f6d3105065b6aea3989e66d06919d3d
JingYongWang <lovedpotato@gmail.com>
2016-07-29 13:10:33 +0800

    Item 1 test OK

--- test/unit/facebook/helper.test.js
+++ test/unit/facebook/helper.test.js
@@ -1,47 +1,50 @@
 //參考 https://github.com/sakuxz/node-tdd-sample
 
-
 import FacebookHelper from '../../../src/facebook/helper.js'
-import task1_initModel from '../../../src/models/friend.js'
+import task1_initModel from '../../../src/database/task1'
 
 describe('facebook-helper', () => {
   let facebookHelper = null;
   let models = null;
 
 
-  before((done) => {
+  before( async (done) => {
+    //自己的ID和Token
     let userId = "648070781944220";
-    let token = "EAACEdEose0cBAOOvpn09BDHRyBakotSsbsljJJvS4IYzduLbAj6rhFZARrnd1lt7a7OU1qleZC5nDBKEUuyKilqYYYmjvQGyKunM140IDi4hTq1X5NJGM6HAVsVT1rVzZB2A4WhTRA7RnQu8XjqqQYlJdScQyRwfhSCXoe6iQZDZD";
+    let token = "EAACEdEose0cBAAHM0D60lAxniq1TezCGFI6g9tjX9gxMvovChX9LvUqlcu710EBfLt6ZAtDLQqcyHZBzpKKDFKgbKiQC34wzgEVKdsXnH30MuWSWahFACfIIcrOin0NY8D6RK5yyZCqpRPspYwUlh3uauPQYEYZCE5ECXWtyKdy5PMZC38pI1";
     facebookHelper = new FacebookHelper({userId, token});
+    models = await task1_initModel();
     console.log(facebookHelper);
     done();
   });
   //把從 facebook 取得的 friends list 存入 sequelize 之 friend model (create)
-  it.only("get frineds list and save in database", async (done) => {
+  it("get frineds list and save in database", async (done) => {
      try {
          let friends = await facebookHelper.getFriends();
          console.log("friends",friends);
          (friends != null ).should.be.true;
          friends.should.be.Array;
-         friends[0].should.have.keys('name','id');
+         friends[0].should.have.keys("name","id");
 
-         friends.foreach((e,i) =>{
+         friends.forEach((e,i) => {
             e.facebookId = e.id;
             e.email = e.undefined;
          });
-         await models.friends.bulkCreate(friends);
+
+         await models.friend.bulkCreate(friends);
          done();
 
      } catch (e) {
          done(e);
      }
   });
-  //把從 facebook 取得的 friends list 存入 sequelize 之 friend model (create)
+
+  //原本用 api 取得 friends list 改為透過查詢資料庫的方式 (find)
   it("get friends list database", async (done) => {
     try {
-      let friends = await models.friends.findAll();
+      let friends = await models.friend.findAll();
       friends.should.be.Array;
-      friends[0].toJSON.should.has.keys(
+      friends[0].toJSON().should.has.keys(
           'id',
           'name',
           'email',
@@ -54,7 +57,7 @@ describe('facebook-helper', () => {
       done(e);
     }
   });
-  //原本用 api 取得 friends list 改為透過查詢資料庫的方式 (find)
+
   it("get friends list in database", async (done) => {
     try {
       let friends = await facebookHelper.getFriends();
@@ -70,7 +73,7 @@ describe('facebook-helper', () => {
   //將其中一個 friend 更新其 email 欄位為 hellojs@trunk.studio (update)
   it('update friends emails', async (done) => {
       try {
-        let friend = await models.friends.findOne();
+        let friend = await models.friend.findOne();
         friend.email = 'hellojs@trunk.studio';
         let result = await modele.friends.
         done();
